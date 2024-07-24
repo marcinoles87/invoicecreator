4e3199d4fee66fda6f97a90a7d6d313be2611c0b
marcinoles87 <marcinoles87@gmail.com>
2024-07-24 10:54:06 +0200

    add section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  let [weight , setWeight] = useState('0'); 
+  let [height , setHeight] = useState('0');
+  const [bmi , setBmi ] = useState('');
+
+  const handleSubmit = () => {
+
+    if(weight > 0){
+      setBmi(weight/(height*height))
+        console.log(bmi)    
+    }else{
+      alert('weight 0 please enter your weight')
+    }
+   
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className='header'>
+        <h1>BMI CALCULATOR</h1>
+      </div>
+
+      <div className='inputs'>
+        <input type="number" placeholder='weight' onChange={ (e) => setWeight(e.target.value)}></input>
+        <input type="number" placeholder='height' onChange={ (e) => setHeight(e.target.value)}></input>
+
+        <p> Example : 70kg /  (1.70m * 1.70m) * 2 </p>
+      </div>
+
+      <div className='submit'>
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+
+      <div className='bmi'>
+        <p>{bmi}</p>
+
+      </div>
+    
+
+      
     </div>
   );
 }
